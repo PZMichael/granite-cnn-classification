afaa5c816a5d1455cefb5be2a0858a992f7e010f
Anselmo Ferreira <anselmo.ferreira@gmail.com>
2017-06-12 20:42:29 +0800

    Solving problems with folder names in Windows

---
+++ calculate_final_statistics.m
@@ -0,0 +1,36 @@
+function [accuracy,precision_vector,recall_vector,f_measure_vector]=calculate_final_statistics(predict, ground_truth, name_file)
+
+	
+	confusion=write_confusion_matrix(predict, ground_truth, name_file);
+	
+	total_matrix=sum(confusion(:));
+	total_diagonal=0;
+	
+	for j=1:size(matrix,1)
+		
+		total_diagonal=total_diagonal+confusion(j,j);
+	end
+	
+	accuracy=100*(total_diagonal/total_matrix);
+
+	precision_vector=zeros(1,25);
+	recall_vector=zeros(1,25);
+	f_measure_vector=zeros(1,25);
+
+	for l=1:size(confusion,1)
+		precision_vector(l)=confusion(l,l)/sum(confusion(:,l));
+		recall_vector(l)=confusion(l,l)/sum(confusion(l,:));
+		f_measure_vector(l)=2*(precision_vector(l)*recall_vector(l))/(precision_vector(l)+recall_vector(l));	
+	end
+
+function confusion=write_confusion_matrix(predict, groundtruth, name_file)
+	
+	confusion=zeros(25,25);
+
+	for i=1:size(predict,1)
+    		confusion(groundtruth(i,1), predict(i,1))=confusion(groundtruth(i,1), predict(i,1))+1;
+	end
+
+	dlmwrite(name_file,confusion);
+	
+
---
+++ demo_cifar.m
@@ -0,0 +1,19 @@
+disp('Starting Experiments...');
+
+[acc1,precision1,recall1,fmeasure1]=main(1,1,2);
+[acc2,precision2,recall2,fmeasure2]=main(1,2,1);
+[acc3,precision3,recall3,fmeasure3]=main(2,1,2);
+[acc4,precision4,recall4,fmeasure4]=main(2,2,1);
+[acc5,precision5,recall5,fmeasure5]=main(3,1,2);
+[acc6,precision6,recall6,fmeasure6]=main(3,2,1);
+[acc7,precision7,recall7,fmeasure7]=main(4,1,2);
+[acc8,precision8,recall8,fmeasure8]=main(4,2,1);
+[acc9,precision9,recall9,fmeasure9]=main(5,1,2);
+[acc10,precision10,recall10,fmeasure10]=main(5,2,1);
+
+disp('Experiments Finished. Reporting Results.');
+disp(['Mean Accuracy: ' num2str(mean(horzcat(acc1,acc2,acc3,acc4,acc5,acc6,acc7,acc8,acc9,acc10)))]);
+disp(['Mean Precisiom: ' num2str(mean(horzcat(precision1,precision2,precision3,precision4,precision5,precision6,precision7,precision8,precision9,precision10)))]);
+disp(['Mean Recall: ' num2str(mean(horzcat(recall1,recall2,recall3,recall4,recall5,recall6,recall7,recall8,recall9,recall10)))]);
+disp(['Mean F-Measure: ' num2str(mean(horzcat(fmeasure1,fmeasure2,fmeasure3,fmeasure4,fmeasure5,fmeasure6,fmeasure7,fmeasure8,fmeasure9,fmeasure10)))]);
+

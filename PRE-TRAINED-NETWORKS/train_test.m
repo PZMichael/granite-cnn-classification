6fc695b94ab7e50c1e14e9c3da9e5a34e18db22e
Anselmo Ferreira <anselmo.ferreira@gmail.com>
2017-11-03 10:31:11 +0800

    Update train_test.m

--- PRE-TRAINED-NETWORKS/train_test.m
+++ PRE-TRAINED-NETWORKS/train_test.m
@@ -16,31 +16,31 @@ function [acc,precision_vector,recall_vector,fmeasure_vector]=train_test(train_f
 	
 	disp('testing classifier');
 	numExemplars = size(test_vec,1);
-        chunkSize = 1000;
-	k=1:chunkSize:numExemplars;
+    chunkSize = 1000;
+    k=1:chunkSize:numExemplars;
 			
-        output=zeros(1,1);
+    output=zeros(1,1);
 
-     	for l=1:length(k)-1
-    		index1 = k(l);
-    		index2 = k(l+1)-1;
-    		fprintf('classifying exemplars %d to %d\n', index1, index2 );
-    		chunk = test_vec(index1:index2,:);
-    		temp=predict(B,chunk);
-		output=vertcat(output,temp);
-	end
+    for l=1:length(k)-1
+    	index1 = k(l);
+    	index2 = k(l+1)-1;
+    	fprintf('classifying exemplars %d to %d\n', index1, index2 );
+    	chunk = test_vec(index1:index2,:);
+    	temp=predict(B,chunk);
+        output=vertcat(output,temp);
+    end
 	%last bit of data
 	chunk = test_vec(k(end):numExemplars,:);
 	temp=predict(B,chunk);
 	output=vertcat(output,temp);
 	output=output(2:end);
 		
-	num_blocks_per_image=ones(1,500)*2116;
+	num_blocks_per_image=ones(1,8)*2116;  
                  
 	output2=voting(output,num_blocks_per_image);
 	ground_truth=voting(test_class,num_blocks_per_image);
 
-	[acc,precision_vector,recall_vector,fmeasure_vector]=calculate_final_statistics(output2', ground_truth', [num2str(folder) '-' num2str(set_training) '-confusion_matrix.txt']);
+	[acc,precision_vector,recall_vector,fmeasure_vector]=calculate_final_statistics(output2', ground_truth', [num2str(folder) '-' num2str(set_training) '-' num2str(set_testing)  '-confusion_matrix.txt']);
 	
 	disp(['Accuracy after majority voting of image blocks:' num2str(acc) '%']);
 
